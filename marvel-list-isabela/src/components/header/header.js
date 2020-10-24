77dc2c3949608264325c85e7987541acaac884e2
Isabela Santos <isa2014mgspn@gmail.com>
2020-10-24 12:17:23 -0300

    struct react add, material add  and header

---
+++ marvel-list-isabela/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/styles/header.css';
+import Logo from'../../assets/img/logo.png'
+export default function Header(){
+
+return(
+ <div className="container">
+ <img className="marvel-logo" src={Logo} /> 
+ </div>
+)
+
+}
